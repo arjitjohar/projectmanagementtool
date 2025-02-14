6192500f303a8b354cfa8e25255a8364b2b881ec
arjitjohar <arjitjohar@gmail.com>
2025-02-14 18:33:41 -0500

    hot fix to user login

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,8 @@ import { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
-import { Amplify } from "aws-amplify";
-import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 import { useAuthenticator } from "@aws-amplify/ui-react";
-Amplify.configure(outputs);
 
 const client = generateClient<Schema>();
 
@@ -32,7 +29,7 @@ export default function App() {
 
   function createTodo() {
     client.models.Todo.create({
-      content: window.prompt("Todo content"),
+      content: window.prompt("add content here"),
     });
   }
 
@@ -55,7 +52,7 @@ export default function App() {
           Review next steps of this tutorial.
         </a>
       </div>
-      <button onClick={signOut}>Sign out</button>
+      <button onClick={signOut}>Sign out here</button>
 
     </main>
   );
